f6a09f5f2a22aef442bce148930377d55bd35dbc
pdewan <dewan@cs.unc.edu>
2014-10-09 10:38:33 -0400

    Added an observable interface

---
+++ src/difficultyPrediction/featureExtraction/RatioFetauresObservable.java
@@ -0,0 +1,7 @@
+package difficultyPrediction.featureExtraction;
+
+public interface RatioFetauresObservable {
+	void addRatioFeaturesListener(RatioFeaturesListener aListener);
+	void removeRatioFetauresListener(RatioFeaturesListener aListener);
+
+}
