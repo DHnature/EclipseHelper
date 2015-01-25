19e92339d83c9c44830cd800122ea908d9fbbeaf
pdewan <dewan@cs.unc.edu>
2015-01-25 03:05:56 -0500

    Copied code in negotiation handler from super class to change it later

--- src/saros/EclipseHelperNegotiationHandler.java
+++ src/saros/EclipseHelperNegotiationHandler.java
@@ -34,6 +34,11 @@ public class EclipseHelperNegotiationHandler extends NegotiationHandler{
         IncomingSessionNegotiation negotiation) {
         showIncomingInvitationUI(negotiation);
     }
+	 @Override
+	    public void handleIncomingProjectNegotiation(
+	        IncomingProjectNegotiation negotiation) {
+	        showIncomingProjectUI(negotiation);
+	    }
 
 	
 	private void showIncomingInvitationUI(
@@ -78,5 +83,49 @@ public class EclipseHelperNegotiationHandler extends NegotiationHandler{
 	                }
 	        });
 	    }
+	private void showIncomingProjectUI(final IncomingProjectNegotiation process) {
+        List<ProjectNegotiationData> pInfos = process.getProjectInfos();
+        final List<FileList> fileLists = new ArrayList<FileList>(pInfos.size());
+
+        for (ProjectNegotiationData pInfo : pInfos)
+            fileLists.add(pInfo.getFileList());
+
+        SWTUtils.runSafeSWTAsync(LOG, new Runnable() {
+
+            @Override
+            public void run() {
+                AddProjectToSessionWizard projectWizard = new AddProjectToSessionWizard(
+                    process, process.getPeer(), fileLists);
+
+                final WizardDialogAccessable wizardDialog = new WizardDialogAccessable(
+                    SWTUtils.getShell(), projectWizard, SWT.MIN | SWT.MAX,
+                    SWT.SYSTEM_MODAL | SWT.APPLICATION_MODAL
+                        | SWT.PRIMARY_MODAL);
+
+                /*
+                 * IMPORTANT: as the dialog is non modal it MUST NOT block on
+                 * open or there is a good chance to crash the whole GUI
+                 * 
+                 * Scenario: A modal dialog is currently open with
+                 * setBlockOnOpen(true) (as most input dialogs are).
+                 * 
+                 * When we now open this wizard with setBlockOnOpen(true) this
+                 * wizard will become the main dispatcher for the SWT Thread. As
+                 * this wizard is non modal you cannot close it because you
+                 * could not access it. Therefore the modal dialog cannot be
+                 * closed as well because it is stuck on the non modal dialog
+                 * which currently serves as main dispatcher !
+                 */
+
+                wizardDialog.setBlockOnOpen(false);
+
+                wizardDialog.setHelpAvailable(false);
+                projectWizard.setWizardDlg(wizardDialog);
+
+                DialogUtils.openWindow(wizardDialog);
+            }
+        });
+    }
+
 
 }
